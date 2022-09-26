4d840ed426a0d319c1d078def6d11ebf3344bfea
lby9906 <lby9906@naver.com>
2022-09-26 14:14:19 +0900

    stash -> commit

--- src/main/java/project/z_v/TestController.java
+++ src/main/java/project/z_v/TestController.java
@@ -2,6 +2,7 @@ package project.z_v;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TestController {
@@ -23,7 +24,7 @@ public class TestController {
         return "testes";
     }
 
-    @GetMapping("/login")
+    @GetMapping ("/login")
     public String login() {
         return "login";
     }
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lby99
+  Date: 2022-09-25
+  Time: 오후 10:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div>
+  메인페이지입니다
+</div>
+
+</body>
+</html>
