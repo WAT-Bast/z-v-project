890cd55bf109e494c37b5e472b7994ba5df795b6
KImSuim <suim7171@naver.com>
2022-11-26 14:10:05 +0900

    allpage 생성

---
+++ src/main/java/project/z_v/BoardRepository.java
@@ -0,0 +1,8 @@
+package project.z_v;
+
+import com.godcoder.myhome.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Hospital, Long>{
+/*public interface BoardRepository extends JpaRepository<Board, long>{*/
+}
--- src/main/java/project/z_v/TestController.java
+++ src/main/java/project/z_v/TestController.java
@@ -1,6 +1,8 @@
 package project.z_v;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.annotation.UserConfigurations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +13,10 @@ import project.z_v.UserDB.User;
 import project.z_v.UserDB.repository.UserRepository;
 
 @Controller
+@RequiredArgsConstructor
 public class TestController {
 
     private final UserRepository userRepository;
-
-    @Autowired
-    public TestController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @GetMapping("/index")
     public String index() {
@@ -72,8 +70,13 @@ public class TestController {
     public String ShopRemove(){
         return "ShopRemove"; }
 
+    @Autowired
+    private  BoardRepository boardRepository;
+
     @GetMapping("/allPage")
-    public String allPage(){
-        return "allPage"; }
+    public String allPage(Model model){
+        List<Hospital> Hospitals = boardRepository.findAll();
+        model.addAttribute("Hospitals",Hospitals);
+        return "bord/allPage"; }
 
 }
