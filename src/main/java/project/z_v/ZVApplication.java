f6b6bf5205e69f9f1171e36e2877d146ce074eaa
KImSuim <suim7171@naver.com>
2022-12-04 02:36:11 +0900

    전체적인거 수정

--- src/main/java/project/z_v/ZVApplication.java
+++ src/main/java/project/z_v/ZVApplication.java
@@ -2,7 +2,9 @@ package project.z_v;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ZVApplication {
 
